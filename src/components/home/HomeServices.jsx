5f309a23466462b4791d2ea26c0433a7a455ad00
Xudoynazar <saparovxudoynazar@gmail.com>
2024-11-23 17:55:01 +0500

    added carousel

--- src/components/home/HomeServices.jsx
+++ src/components/home/HomeServices.jsx
@@ -4,8 +4,8 @@ const HomeServices = () => {
   return (
     <div className="flex flex-col justify-start md:px-28 px-10 py-10 bg-gray-900">
       <p className="text-start">Luxury Cars for Rent in Dubai / Services</p>
-      <div className="flex md:gap-10 gap-5 w-full mt-10">
-        <div className="md:w-[25rem] w-1/2 flex flex-col gap-5 justify-start items-start">
+      <div className="flex md:flex-row flex-col md:gap-10 gap-5 w-full mt-10">
+        <div className="md:w-[25rem] flex flex-col gap-5 justify-start items-start">
           <img src="/cars/service1.jpg" alt="service 1" className="rounded" />
           <p className="md:text-3xl text-xl text-start">
             Ride a buggy in Dubai and have fun
@@ -16,7 +16,7 @@ const HomeServices = () => {
           </p>
           <Link to={"/services/detail"} className="text-white">LEARN MORE</Link>
         </div>
-        <div className="md:w-[25rem] w-1/2 flex flex-col gap-5 justify-start items-start">
+        <div className="md:w-[25rem] flex flex-col gap-5 justify-start items-start">
           <img src="/cars/service2.jpg" alt="service 1" className="rounded" />
           <p className="md:text-3xl text-xl text-start">
             Photoshoot with luxury car rental Dubai
